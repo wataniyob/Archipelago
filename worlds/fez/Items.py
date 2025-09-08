2c6d496d6e907f307bb237a5dd2f81e400c95b69
Naomi Hyldmar <42452088+nhyldmar@users.noreply.github.com>
2025-09-08 19:29:25 +0100

    Modify item data structure initial attempt at item generation

--- worlds/fez/Items.py
+++ worlds/fez/Items.py
@@ -1,100 +1,60 @@
 from BaseClasses import ItemClassification
-from typing import TypedDict, List
+from typing import List
 
 from BaseClasses import Item
 
+base_id = 0xFE500
 
 class FezItem(Item):
-    name: str = "Fez"
+    game: str = "Fez"
 
 
-class ItemDict(TypedDict):
-    name: str
-    count: int
-    classification: ItemClassification
+class FezItemData():
+    def __init__(self, name: str, count: int, classification: ItemClassification):
+        self.name = name
+        self.count = count
+        self.classification = classification
 
 
-progression_items: List[ItemDict] = [
-    {'name': "Golden Cube",
-     'count': 32,
-     'classification': ItemClassification.progression},
-    {'name': "Anti-Cube",
-     'count': 32,
-     'classification': ItemClassification.progression},
-    {'name': "Key",
-     'count': 8,
-     'classification': ItemClassification.progression},
-    {'name': "Owl",
-     'count': 4,
-     'classification': ItemClassification.progression_deprioritized}
+progression_items: List[FezItemData] = [
+    FezItemData("Golden Cube", 32, ItemClassification.progression),
+    FezItemData("Anti-Cube", 32, ItemClassification.progression),
+    FezItemData("Key", 8, ItemClassification.progression),
+    FezItemData("Owl", 4, ItemClassification.progression_deprioritized)
 ]
 
-skippable_items: List[ItemDict] = [
-    {'name': "Red Map",
-     'count': 1,
-     'classification': ItemClassification.deprioritized},
-    {'name': "Purple Map",
-     'count': 1,
-     'classification': ItemClassification.deprioritized},
-    {'name': "Tower Map",
-     'count': 1,
-     'classification': ItemClassification.deprioritized},
-    {'name': "QR Code Map",
-     'count': 1,
-     'classification': ItemClassification.deprioritized},
-    {'name': "Burned Map",
-     'count': 1,
-     'classification': ItemClassification.deprioritized},
-    {'name': "Cemetery Map 1",
-     'count': 1,
-     'classification': ItemClassification.deprioritized},
-    {'name': "Cemetery Map 2",
-     'count': 1,
-     'classification': ItemClassification.deprioritized},
-    {'name': "Cemetery Map 3",
-     'count': 1,
-     'classification': ItemClassification.deprioritized},
-    {'name': "Cemetery Map 4",
-     'count': 1,
-     'classification': ItemClassification.deprioritized},
-    {'name': "The Writing Cube",
-     'count': 1,
-     'classification': ItemClassification.deprioritized},
-    {'name': "The Counting Cube",
-     'count': 1,
-     'classification': ItemClassification.deprioritized},
-    {'name': "The Tome Artifact",
-     'count': 1,
-     'classification': ItemClassification.deprioritized},
-    {'name': "The Skull Artifact",
-     'count': 1,
-     'classification': ItemClassification.deprioritized},
-    {'name': "Heart Cube",
-     'count': 3,
-     'classification': ItemClassification.deprioritized}
+skippable_items: List[FezItemData] = [
+    FezItemData("Red Map", 1, ItemClassification.deprioritized),
+    FezItemData("Purple Map", 1, ItemClassification.deprioritized),
+    FezItemData("Tower Map", 1, ItemClassification.deprioritized),
+    FezItemData("QR Code Map", 1, ItemClassification.deprioritized),
+    FezItemData("Burned Map", 1, ItemClassification.deprioritized),
+    FezItemData("Cemetery Map 1", 1, ItemClassification.deprioritized),
+    FezItemData("Cemetery Map 2", 1, ItemClassification.deprioritized),
+    FezItemData("Cemetery Map 3", 1, ItemClassification.deprioritized),
+    FezItemData("Cemetery Map 4", 1, ItemClassification.deprioritized),
+    FezItemData("The Writing Cube", 1, ItemClassification.deprioritized),
+    FezItemData("The Counting Cube", 1, ItemClassification.deprioritized),
+    FezItemData("The Tome Artifact", 1, ItemClassification.deprioritized),
+    FezItemData("The Skull Artifact", 1, ItemClassification.deprioritized),
+    FezItemData("Heart Cube", 3, ItemClassification.deprioritized)
 ]
 
-trap_items: List[ItemDict] = [
-    {'name': "Rotation Trap",
-     'count': 1,
-     'classification': ItemClassification.trap},
-    {'name': "Sleep Trap",
-     'count': 1,
-     'classification': ItemClassification.trap}
+trap_items: List[FezItemData] = [
+    FezItemData("Rotation Trap", 5, ItemClassification.trap),
+    FezItemData("Sleep Trap", 5, ItemClassification.trap)
 ]
 
-filler_items: List[ItemDict] = [
-    {'name': "Emotional Support",
-     'count': 1,
-     'classification': ItemClassification.filler}
+filler_items: List[FezItemData] = [
+    FezItemData("Emotional Support", 1, ItemClassification.filler)
 ]
 
-all_items = progression_items + skippable_items + trap_items + filler_items
+all_fez_item_data = progression_items + skippable_items + trap_items + filler_items
 
-item_names = {item['name'] for item in all_items}
+item_names = {item.name for item in all_fez_item_data}
 item_name_groups = {
-    "Progression":  {item['name'] for item in progression_items},
-    "Skippable":    {item['name'] for item in skippable_items},
-    "Trap":         {item['name'] for item in trap_items},
-    "Filer":        {item['name'] for item in filler_items},
+    "Progression":  {item.name for item in progression_items},
+    "Skippable":    {item.name for item in skippable_items},
+    "Trap":         {item.name for item in trap_items},
+    "Filer":        {item.name for item in filler_items},
 }
