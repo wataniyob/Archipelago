4ffab2af58766bb822c4e657bf5f8f8a3b9ff4d9
Naomi Hyldmar <42452088+nhyldmar@users.noreply.github.com>
2025-09-19 12:55:17 +0100

    Add some initial code for unique locations

--- worlds/fez/__init__.py
+++ worlds/fez/__init__.py
@@ -1,7 +1,7 @@
 from typing import Any, Dict
 from .Options import FezOptions
-from .Items import FezItem, main_items, item_name_groups, trap_items, filler_items
-from .Locations import location_names, location_name_groups
+from .Items import FezItem, all_items, item_name_groups, trap_items, filler_items
+from .Locations import all_location_data, location_name_groups
 from .Rules import set_all_rules
 from worlds.AutoWorld import WebWorld, World
 from BaseClasses import Item, Tutorial
@@ -36,23 +36,19 @@ class FezWorld(World):
     topology_present = True  # show path to required location checks in spoiler
 
     # First item and location ID
-    base_id = 0xFE500
+    _base_id = 0xFE500
 
-    all_item_data = main_items + trap_items + filler_items
-    item_name_to_id = {item.name: id for id, item in enumerate(all_item_data, base_id)}
+    item_name_to_id = {item.name: id for id, item in enumerate(all_item_data, _base_id)}
+    item_names = item_name_to_id.keys()
     item_name_groups = item_name_groups
 
-    # TODO: Figure out why no items get listed in generation
-    location_name_to_id = {name: id for id, name in enumerate(location_names, base_id)}
+    location_name_to_id = {name: id for id, name in enumerate(all_location_data, _base_id)}
+    location_names = location_name_to_id.keys()
     location_name_groups = location_name_groups
 
-    def get_filler_item_name(self) -> str:
-        return self.random.choice(filler_items).name
-
-    def create_item(self, name: str) -> Item:
-        item_id = self.item_name_to_id[name]
-        item_data = self.all_item_data[item_id - self.base_id]
-        return FezItem(name, item_data.classification, item_id, self.player)
+    def create_regions(self) -> None:
+        # TODO: Add regions to self.multiworld
+        pass
 
     def create_items(self) -> None:
         for item in main_items:
@@ -68,9 +64,9 @@ class FezWorld(World):
     def set_rules(self) -> None:
         set_all_rules(self)
 
-        # Uncomment to visualise world layout
-        # from Utils import visualize_regions
-        # visualize_regions(self.multiworld.get_region("Menu", self.player), "fez_chart.puml")
+    def connect_entrances(self) -> None:
+        # TODO: Add connections to self.multiworld
+        pass
 
     def fill_slot_data(self) -> Dict[str, Any]:
         return self.options.as_dict(
@@ -79,3 +75,21 @@ class FezWorld(World):
             "disable_visual_pain"
         )
 
+    def create_item(self, name: str) -> Item:
+        item_id = self.item_name_to_id[name]
+        item_data = self.all_item_data[item_id - self._base_id]
+        return FezItem(name, item_data.classification, item_id, self.player)
+
+    def get_filler_item_name(self) -> str:
+        return self.random.choice(filler_items).name
+
+    def get_location(self, location_name: str) -> Location:
+        location_data = self.all_location_data[location_name]
+        region = self.multiworld.regions[location_data.region]
+        return FezLocation(self.player, location_data.name, region)
+
+    def get_locations(self) -> Iterable[Location]:
+        all_locations = []
+        for location in all_location_data:
+            all_locations.append(self.get_location(location.name))
+        return all_locations
