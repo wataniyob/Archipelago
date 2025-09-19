8931597ce551ef16c21fd121b64db7380e969249
Naomi Hyldmar <42452088+nhyldmar@users.noreply.github.com>
2025-09-19 12:54:44 +0100

    Add sunglasses as item

--- worlds/fez/Items.py
+++ worlds/fez/Items.py
@@ -15,39 +15,41 @@ class FezItemData():
 
 
 progression_items: List[FezItemData] = [
-    FezItemData("Golden Cube", 32, ItemClassification.progression),
-    FezItemData("Anti-Cube", 32, ItemClassification.progression),
-    FezItemData("Key", 8, ItemClassification.progression),
-    FezItemData("Owl", 4, ItemClassification.progression_deprioritized)
+    FezItemData("Golden Cube",          32, ItemClassification.progression),
+    FezItemData("Anti-Cube",            32, ItemClassification.progression),
+    FezItemData("Key",                  8,  ItemClassification.progression),
+    FezItemData("Owl",                  4,  ItemClassification.progression_deprioritized)
 ]
 
 skippable_items: List[FezItemData] = [
-    FezItemData("Red Map", 1, ItemClassification.deprioritized),
-    FezItemData("Purple Map", 1, ItemClassification.deprioritized),
-    FezItemData("Tower Map", 1, ItemClassification.deprioritized),
-    FezItemData("QR Code Map", 1, ItemClassification.deprioritized),
-    FezItemData("Burned Map", 1, ItemClassification.deprioritized),
-    FezItemData("Cemetery Map 1", 1, ItemClassification.deprioritized),
-    FezItemData("Cemetery Map 2", 1, ItemClassification.deprioritized),
-    FezItemData("Cemetery Map 3", 1, ItemClassification.deprioritized),
-    FezItemData("Cemetery Map 4", 1, ItemClassification.deprioritized),
-    FezItemData("The Writing Cube", 1, ItemClassification.deprioritized),
-    FezItemData("The Counting Cube", 1, ItemClassification.deprioritized),
-    FezItemData("The Tome Artifact", 1, ItemClassification.deprioritized),
-    FezItemData("The Skull Artifact", 1, ItemClassification.deprioritized),
-    FezItemData("Heart Cube", 3, ItemClassification.deprioritized)
+    FezItemData("Red Map",              1,  ItemClassification.deprioritized),
+    FezItemData("Purple Map",           1,  ItemClassification.deprioritized),
+    FezItemData("Tower Map",            1,  ItemClassification.deprioritized),
+    FezItemData("QR Code Map",          1,  ItemClassification.deprioritized),
+    FezItemData("Burned Map",           1,  ItemClassification.deprioritized),
+    FezItemData("Cemetery Map 1",       1,  ItemClassification.deprioritized),
+    FezItemData("Cemetery Map 2",       1,  ItemClassification.deprioritized),
+    FezItemData("Cemetery Map 3",       1,  ItemClassification.deprioritized),
+    FezItemData("Cemetery Map 4",       1,  ItemClassification.deprioritized),
+    FezItemData("The Writing Cube",     1,  ItemClassification.deprioritized),
+    FezItemData("The Counting Cube",    1,  ItemClassification.deprioritized),
+    FezItemData("The Tome Artifact",    1,  ItemClassification.deprioritized),
+    FezItemData("The Skull Artifact",   1,  ItemClassification.deprioritized),
+    FezItemData("Heart Cube",           3,  ItemClassification.deprioritized),
+    FezItemData("Sunglasses",           1,  ItemClassification.deprioritized)
 ]
 
 trap_items: List[FezItemData] = [
-    FezItemData("Rotation Trap", 1, ItemClassification.trap),
-    FezItemData("Sleep Trap", 1, ItemClassification.trap)
+    FezItemData("Rotation Trap",        1,  ItemClassification.trap),
+    FezItemData("Sleep Trap",           1,  ItemClassification.trap)
 ]
 
 filler_items: List[FezItemData] = [
-    FezItemData("Emotional Support", 1, ItemClassification.filler)
+    FezItemData("Emotional Support",    1,  ItemClassification.filler)
 ]
 
 main_items = progression_items + skippable_items
+all_item_data = main_items + trap_items + filler_items
 
 item_name_groups = {
     "Progression":  {item.name for item in progression_items},
