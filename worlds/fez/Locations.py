5006013a7182a66661ab294039da0788acb3942d
Naomi Hyldmar <42452088+nhyldmar@users.noreply.github.com>
2025-09-19 12:55:17 +0100

    Add some initial code for unique locations

--- worlds/fez/Locations.py
+++ worlds/fez/Locations.py
@@ -1,49 +1,75 @@
-# TODO: Name each bit, cube and collectible uniquely
-
-from typing import Set
+from typing import List, Set
 from BaseClasses import Location
 
 
 class FezLocation(Location):
     game: str = "Fez"
 
+class FezLocationData:
+    def __init__(self, name: str, region: str):
+        self.name = name
+        self.region = count
 
-artifact_locations = [
-    "The Writing Cube",
-    "The Counting Cube",
-    "The Tome Artifact",
-    "The Skull Artifact"
-]
 
-map_locations = [
-    "Red Map"
-    "Purple Map"
-    "Tower Map"
-    "QR Code Map"
-    "Burned Map"
-    "Cemetery Map 1"
-    "Cemetery Map 2"
-    "Cemetery Map 3"
-    "Cemetery Map 4"
-]
+cube_bit_locations = {
+    # TODO: Add all 64 cube bit locations
+}
 
-location_names: Set[str] = set()
-location_names.union(artifact_locations)
-location_names.union(map_locations)
-location_names.union([f"Cube Bit {i}" for i in range(1, 65)])       # 64 Cube Bits
-location_names.union([f"Golden Cube {i}" for i in range(1, 25)])    # 24 Golden Cubes
-location_names.union([f"Anti-Cube {i}" for i in range(1, 32)])      # 32 Anti-Cubes
-location_names.union([f"Heart Cube {i}" for i in range(1, 4)])      # 3 Heart Cubes
-location_names.union([f"Key {i}" for i in range(1, 9)])             # 8 Keys
-location_names.union([f"Owl {i}" for i in range(1, 5)])             # 4 Owls
+golden_cube_locations = {
+    # TODO: Add all 24 golden cube locations
+}
+
+anti_cube_locations = {
+    # TODO: Add all 32 anti-cube locations
+}
+
+heart_cube_locations = {
+    # TODO: Add all 3 heart cube locations
+}
+
+key_locations = {
+    # TODO: Add all 8 key locations
+}
+
+owl_locations = {
+    # TODO: Add all 4 owl locations
+}
+
+artifact_locations = {
+    FezLocationData("The Writing Cube",     "TODO"),
+    FezLocationData("The Counting Cube",    "TODO"),
+    FezLocationData("The Tome Artifact",    "TODO"),
+    FezLocationData("The Skull Artifact",   "TODO"),
+}
+
+map_locations = {
+    FezLocationData("Red Map"               "TODO"),
+    FezLocationData("Purple Map"            "TODO"),
+    FezLocationData("Tower Map"             "TODO"),
+    FezLocationData("QR Code Map"           "TODO"),
+    FezLocationData("Burned Map"            "TODO"),
+    FezLocationData("Cemetery Map 1"        "TODO"),
+    FezLocationData("Cemetery Map 2"        "TODO"),
+    FezLocationData("Cemetery Map 3"        "TODO"),
+    FezLocationData("Cemetery Map 4"        "TODO"),
+}
+
+all_locations = cube_bit_locations \
+                + golden_cube_locations \
+                + anti_cube_locations \
+                + heart_cube_locations \
+                + key_locations \
+                + owl_locations \
+                + artifact_locations \
+                + map_locations
 
 location_name_groups = {
-    "Cube Bit":     {location for location in location_names if "Cube Bit" in location},
-    "Golden Cube":  {location for location in location_names if "Golden Cube" in location},
-    "Anti-Cube":    {location for location in location_names if "Anti-Cube" in location},
-    "Heart Cube":   {location for location in location_names if "Heart Cube" in location},
-    "Key":          {location for location in location_names if "Key" in location},
-    "Owl":          {location for location in location_names if "Owl" in location},
-    "Artifact":     set(artifact_locations),
-    "Map":          set(map_locations)
+    "Cube Bit":     cube_bit_locations,
+    "Golden Cube":  golden_cube_locations,
+    "Anti-Cube":    anti_cube_locations,
+    "Heart Cube":   heart_cube_locations,
+    "Key":          key_locations,
+    "Owl":          owl_locations,
+    "Artifact":     artifact_locations,
+    "Map":          map_locations,
 }
